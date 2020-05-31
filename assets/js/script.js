f4602a2467ee01017fc92c31f7faf9c669de1c7f
JustinMcC066 <carthymcjustin@gmail.com>
2020-05-31 20:49:04 +0100

    Add spinning vinyl animation to Start Game click function so it is not running in the background of the landing screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 // Remove landing screen to show main game area when "Start Game" button is clicked 
+
 $("#start-game").click(function () {
     $("#landing").removeClass("d-flex");
     $("#landing").addClass("d-none");
+    // Display spinning vinyl and needle arm animation 
+    $("#spinningVinyl").removeClass("d-none");
+    $("#spinningVinyl").addClass("d-flex");
+});
+
+// Return to home screen when drum console home icon is clicked 
+
+$(".home-btn").click(function () {
+    window.location.reload()
 });
