337f6187b69a6d492e6089b574d114585cd8209a
JustinMcC066 <carthymcjustin@gmail.com>
2020-06-01 15:27:13 +0100

    Add click functions to show and hide pad groups when instrument transport buttons are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*--------------------------------- Landing Screen ------------------------------*/
+
 // Remove landing screen to show main game area when "Start Game" button is clicked 
 
 $("#start-game").click(function () {
@@ -13,48 +15,88 @@ $("#start-game").click(function () {
     $(".parallax").addClass("d-none");
 });
 
+// Landing screen dust particle parallax effect
+// Based on source: https://codepen.io/SabAsan/pen/gOaRJzV
+
+function Parallax(options) {
+    options = options || {};
+    this.nameSpaces = {
+        wrapper: options.wrapper || '.parallax',
+        layers: options.layers || '.parallax-layer',
+        deep: options.deep || 'data-parallax-deep'
+    };
+    this.init = function () {
+        var self = this,
+            parallaxWrappers = document.querySelectorAll(this.nameSpaces.wrapper);
+        for (var i = 0; i < parallaxWrappers.length; i++) {
+            (function (i) {
+                parallaxWrappers[i].addEventListener('mousemove', function (e) {
+                    var x = e.clientX,
+                        y = e.clientY,
+                        layers = parallaxWrappers[i].querySelectorAll(self.nameSpaces.layers);
+                    for (var j = 0; j < layers.length; j++) {
+                        (function (j) {
+                            var deep = layers[j].getAttribute(self.nameSpaces.deep),
+                                disallow = layers[j].getAttribute('data-parallax-disallow'),
+                                itemX = (disallow && disallow === 'x') ? 0 : x / deep,
+                                itemY = (disallow && disallow === 'y') ? 0 : y / deep;
+                            if (disallow && disallow === 'both') return;
+                            layers[j].style.transform = 'translateX(' + itemX + '%) translateY(' + itemY + '%)';
+                        })(j);
+                    }
+                })
+            })(i);
+        }
+    };
+    this.init();
+    return this;
+}
+window.addEventListener('load', function () {
+    new Parallax();
+});
+
+/*--------------------------------- Drum Console ------------------------------*/
+
+//Hide and show pads
+
+//Show kick pads only on load
+$(".snare-pads, .hihat-pads, .openhh-pads").hide();
+
+//Show kick pads only when clicked
+$("#kick").click(function () {
+    $(".snare-pads, .hihat-pads, .openhh-pads").hide();
+    $(".kick-pads").show();
+    $("#kick").addClass("trigger");
+    $("#hi-hats, #snare, #open-hh").removeClass("trigger");
+});
+
+//Show snare pads only when clicked
+$("#snare").click(function () {
+    $(".kick-pads, .hihat-pads, .openhh-pads").hide();
+    $(".snare-pads").show();
+    $("#snare").addClass("trigger");
+    $("#hi-hats, #open-hh, #kick").removeClass("trigger");
+});
+
+//Show hi-hat pads only when clicked
+$("#hi-hats").click(function () {
+    $(".snare-pads, .kick-pads, .openhh-pads").hide();
+    $(".hihat-pads").show("show");
+    $("#hi-hats").addClass("trigger");
+    $("#open-hh, #snare, #kick").removeClass("trigger");
+});
+
+//Show open hi-hat pads only when clicked
+$("#open-hh").click(function () {
+    $(".snare-pads, .hihat-pads, .kick-pads").hide();
+    $(".openhh-pads").show("show");
+    $("#open-hh").addClass("trigger");
+    $("#hi-hats, #snare, #kick").removeClass("trigger");
+});
+
+
 // Return to home screen when drum console home icon is clicked 
 
 $(".home-btn").click(function () {
     window.location.reload()
 });
-
-// Landing screen dust particle parallax effect
-// Based on source: https://codepen.io/SabAsan/pen/gOaRJzV
-
-function Parallax(options){
-    options = options || {};
-    this.nameSpaces = {
-      wrapper: options.wrapper || '.parallax',
-      layers: options.layers || '.parallax-layer',
-      deep: options.deep || 'data-parallax-deep'
-    };
-    this.init = function() {
-      var self = this,
-          parallaxWrappers = document.querySelectorAll(this.nameSpaces.wrapper);
-      for(var i=0; i< parallaxWrappers.length; i++) {
-        (function(i) {
-          parallaxWrappers[i].addEventListener('mousemove', function(e){
-            var x = e.clientX,
-                y = e.clientY,
-                layers = parallaxWrappers[i].querySelectorAll(self.nameSpaces.layers);
-            for(var j=0; j<layers.length; j++){
-               (function(j){
-                 var deep = layers[j].getAttribute(self.nameSpaces.deep),
-                     disallow = layers[j].getAttribute('data-parallax-disallow'),
-                     itemX = (disallow && disallow === 'x') ? 0 : x / deep,
-                     itemY = (disallow && disallow === 'y') ? 0 : y / deep;
-                 if(disallow && disallow === 'both') return;
-                 layers[j].style.transform = 'translateX(' + itemX + '%) translateY(' + itemY + '%)';
-               })(j);
-            }
-          })
-        })(i);
-      }
-    };
-    this.init();
-    return this;
-  }
-  window.addEventListener('load', function(){
-    new Parallax();
-  });
