d259a6314599b682b798dd1f96c492638cc07208
JustinMcC066 <carthymcjustin@gmail.com>
2020-06-20 15:43:45 +0100

    Remove shots variable update from resetConsole function

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,14 +183,9 @@ function youLose() {
     };
 };
 
-
 // Reset
 function resetConsole() {
-    // Resets shots to retry value so minusShots() function begins at original number 
-    // Resets the counter to the correct value for each difficulty
     // Plays vinyl back spin sound
-    shots = retry;
-    $(".digits").text(retry);
     sound.play('backSpin');
     // Clears all pads when reset
     $(".kick-drum, .snare-drum, .hihat-drum, .openhh-drum").removeClass("correct wrong");
